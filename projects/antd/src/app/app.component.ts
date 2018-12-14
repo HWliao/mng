d5c0ab22bac7c47904423554de43870fd8b31141
HWliao <499606788@qq.com>
2018-12-14 23:17:17 +0800

    feat: ng add antd

---
+++ projects/antd/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mz-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'antd';
+}
